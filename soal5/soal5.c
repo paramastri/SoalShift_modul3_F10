ede086b5933fac25d3b34203152fa04ffb49d7a5
Paramastri Ardiningrum <37019687+paramastri@users.noreply.github.com>
2019-04-13 05:38:10 +0700

    Update soal5.c

--- soal5/soal5.c
+++ soal5/soal5.c
@@ -41,22 +41,22 @@ char getch(void)
   return getch_(0);
 }
 
-char nama[100];
-int statusLapar=200;
-int statusBersih=100;
-int statusSehat=300;
-int detik=20;
-int detikSehat=10;
-int detikBersih=30;
-int detikLapar=10;
-int siapMandi=0;
+char monster[800];
+int Hung_Stat=200;
+int Hyg_Stat=100;
+int Health_Stat=300;
+int det_mandi=20;
+int det_health=10;
+int det_hyg=30;
+int det_hung=10;
+int langsung_mandi=0;
 int jeda=0;
-pthread_t tid[7];
-char pil;
-int makanan=0;
-void* menu(void*);
+pthread_t thread[7];
+char choose;
+int maem_monster=0;
+void* tampilan_asli(void*);
 
-void* kelaparan (void *ptr)
+void* laper (void *ptr)
 {
     while(1)
     {
@@ -66,20 +66,20 @@ void* kelaparan (void *ptr)
 
         }
         sleep(1);
-        if(detikLapar==1)
+        if(det_hung==1)
         {
-        	// berkurang 5 tiap 10 detik
-            detikLapar=10;
-            statusLapar-=5;
+        	// berkurang 5 tiap 10 det_hungry
+            det_hung=10;
+            Hung_Stat-=5;
         }
-        else detikLapar-=1;
+        else det_hung-=1;
         if(jeda==0)
         {
 
 
-            if(statusLapar<=0) // hunger status = 0
+            if(Hung_Stat<=0) // hunger status = 0
             {
-                printf("Monster mati karena kelaparan\n");
+                printf("Kelaperan. Mati deh :(\n");
                 exit(EXIT_FAILURE);
             }
 
@@ -89,7 +89,7 @@ void* kelaparan (void *ptr)
     }
 }
 
-void* kebersihan (void *ptr)
+void* hyginis (void *ptr)
 {
     while(1)
     {
@@ -99,18 +99,18 @@ void* kebersihan (void *ptr)
 
         }
         sleep(1);
-        if(detikBersih==1)
+        if(det_hyg==1)
         {
-            detikBersih=30;
-            statusBersih-=10;
+            det_hyg=30;
+            Hyg_Stat-=10;
         }
-        else detikBersih-=1;
+        else det_hyg-=1;
         if(jeda==0)
         {
 
-            if(statusBersih<=0)
+            if(Hyg_Stat<=0)
             {
-                printf("Monster mati karena terkena terlalu kotor\n");
+                printf("Kotor banget. Monster dah mati\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -118,7 +118,7 @@ void* kebersihan (void *ptr)
     }
 }
 
-void* kesehatan (void *ptr)
+void* sehat (void *ptr)
 {
     while(1)
     {
@@ -127,19 +127,19 @@ void* kesehatan (void *ptr)
 
         }
         sleep(1);
-        if(detikSehat==1)
+        if(det_health==1)
         {
-            detikSehat=10;
-            statusSehat+=5;
+            det_health=10;
+            Health_Stat+=5;
         }
-        else detikSehat-=1;
+        else det_health-=1;
         if(jeda==0)
         {
 
 
-            if(statusSehat<=0)
+            if(Health_Stat<=0)
             {
-                printf("Monster mati karena sakit-sakitan\n");
+                printf("Monster mati. Sakit-sakitan sih\n");
                 exit(EXIT_FAILURE);
             }
 
@@ -148,7 +148,7 @@ void* kesehatan (void *ptr)
     }
 }
 
-void* countdown (void *ptr)
+void* itungan_mandi (void *ptr)
 {
     while(1)
     {
@@ -156,30 +156,33 @@ void* countdown (void *ptr)
         {
 
         }
-        if(siapMandi==0)
+        if(langsung_mandi==0)
         {
             sleep(1);
-            if(detik==0)
+            if(det_mandi==0)
             {
-                detik=20;
-                siapMandi=1;
+                det_mandi=20;
+                langsung_mandi=1;
             }
-            else detik-=1;
+            else det_mandi-=1;
         }
 
     }
 }
-int *jumMakan;
-void* toko (void* arg){
+
+int *maem_shop;
+void* ini_shop (void* arg){
     key_t key = 1234;
 
 
     int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-    jumMakan = shmat(shmid, NULL, 0);
+    maem_shop = shmat(shmid, NULL, 0);
 
-    *jumMakan = 0;
+    *maem_shop = 0;
 }
-void* menu(void *arg)
+
+
+void* tampilan_asli(void *arg)
 {
     while(1){
     while(jeda!=0)
@@ -188,13 +191,13 @@ void* menu(void *arg)
     }
     system("clear");
     printf("Standby Mode\n");
-    printf("nama : %s\n",nama);
-    printf("Health : %d\n",statusSehat);
-    printf("Hunger : %d\n",statusLapar);
-    printf("Hygiene : %d\n",statusBersih);
-    printf("Food Left : %d\n",makanan);
-    if(siapMandi==0)
-    printf("Bath will be ready in %d\n",detik);
+    printf("monster : %s\n",monster);
+    printf("Health : %d\n",Health_Stat);
+    printf("Hunger : %d\n",Hung_Stat);
+    printf("Hygiene : %d\n",Hyg_Stat);
+    printf("Food Left : %d\n",maem_monster);
+    if(langsung_mandi==0)
+    printf("Bath will be ready in %d\n",det_mandi);
     else printf("Bath is ready\n");
     printf("Choice :\n");
     printf("1. Eat\n");
@@ -206,7 +209,7 @@ void* menu(void *arg)
     }
 }
 
-void* menu2(void *arg)
+void* tampilan_food(void *arg)
 {
     while(1){
     while(jeda!=2)
@@ -215,8 +218,8 @@ void* menu2(void *arg)
     }
     system("clear");
     printf("Shop Mode\n");
-    printf("Shop food stock : %d\n",*jumMakan);
-    printf("Your food stock : %d\n",makanan);
+    printf("Shop food stock : %d\n",*maem_shop);
+    printf("Your food stock : %d\n",maem_monster);
     printf("Choices\n");
     printf("1. Buy\n");
     printf("2. Back\n");
@@ -228,53 +231,53 @@ int main()
 {
     char c;
     int i;
-    printf("Beri nama monstermu > ");
+    printf("Beri monster monstermu > ");
     c=getchar();
     while(c!='\n')
     {
-        nama[i]=c;
+        monster[i]=c;
         c=getchar();
         i++;
     }
-    pthread_create(&(tid[0]),NULL,kelaparan,NULL);
-    pthread_create(&(tid[1]),NULL,kebersihan,NULL);
-    pthread_create(&(tid[2]),NULL,kesehatan,NULL);
-    pthread_create(&(tid[3]),NULL,countdown,NULL);
-    pthread_create(&(tid[4]),NULL,menu,NULL);
-    pthread_create(&(tid[5]),NULL,toko,NULL);
-    pthread_create(&(tid[6]),NULL,menu2,NULL);
+    pthread_create(&(thread[0]),NULL,laper,NULL);
+    pthread_create(&(thread[1]),NULL,hyginis,NULL);
+    pthread_create(&(thread[2]),NULL,sehat,NULL);
+    pthread_create(&(thread[3]),NULL,itungan_mandi,NULL);
+    pthread_create(&(thread[4]),NULL,tampilan_asli,NULL);
+    pthread_create(&(thread[5]),NULL,ini_shop,NULL);
+    pthread_create(&(thread[6]),NULL,tampilan_food,NULL);
 
     while(1)
     {
-        // menu();
-        pil=getch();
-        if(pil=='1')
+        // tampilan_asli();
+        choose=getch();
+        if(choose=='1')
         {
-            if(makanan>0)
+            if(maem_monster>0)
             {
-                statusLapar+=15;
-                makanan--;
-                if(statusLapar>200)
+                Hung_Stat+=15;
+                maem_monster--;
+                if(Hung_Stat>200)
 	            {
-	                statusLapar=200;
+	                Hung_Stat=200;
 	            }
             }
 
         }
-        else if(pil=='2')
+        else if(choose=='2')
         {
-            if(siapMandi==1)
+            if(langsung_mandi==1)
             {
-                statusBersih+=30;
-                if(statusBersih>100)
+                Hyg_Stat+=30;
+                if(Hyg_Stat>100)
                 {
-                    statusBersih=100;
+                    Hyg_Stat=100;
                 }
-                siapMandi=0;
+                langsung_mandi=0;
             }
 
         }
-        else if(pil=='3')
+        else if(choose=='3')
         {
             int statusLawan=100;
             jeda=1;
@@ -282,26 +285,26 @@ int main()
             {
                 system("clear");
                 printf("Battle Mode\n");
-                printf("Monster's Health : %d\n",statusSehat);
+                printf("Monster's Health : %d\n",Health_Stat);
                 printf("Enemy's Health : %d\n",statusLawan);
                 printf("Choices\n");
                 printf("1. Attack\n");
                 printf("2. Run\n");
-                char charac;
-                charac = getch();
-                if(charac=='1')
+                char pilih;
+                pilih = getch();
+                if(pilih=='1')
                 {
-                    if(statusSehat>0 && statusLawan>0)
+                    if(Health_Stat>0 && statusLawan>0)
                     {
-                        statusSehat-=20;
+                        Health_Stat-=20;
                         if(statusLawan>0)
                         {
                             statusLawan-=20;
                         }
                     }
-                    if(statusSehat<0)
+                    if(Health_Stat<0)
                     {
-                        statusSehat=0;
+                        Health_Stat=0;
                         jeda=0;
                         break;
                     }
@@ -310,7 +313,7 @@ int main()
                         statusLawan=0;
                     }
                 }
-                else if(charac=='2')
+                else if(pilih=='2')
                 {
                     jeda=0;
                     break;
@@ -319,23 +322,23 @@ int main()
 
             }
         }
-        else if(pil=='4')
+        else if(choose=='4')
         {
             jeda=2;
-            char charac;
+            char pilih;
             while(1)
             {
-                charac = getch();
-                if(charac=='1')
+                pilih = getch();
+                if(pilih=='1')
                 {
-                    if(*jumMakan > 0)
+                    if(*maem_shop > 0)
                     {
-                        *jumMakan = *jumMakan - 1;
-                        makanan++;
+                        *maem_shop = *maem_shop - 1;
+                        maem_monster++;
                     }
 
                 }
-                else if(charac=='2')
+                else if(pilih=='2')
                 {
                     jeda=0;
                     break;
@@ -344,20 +347,20 @@ int main()
 
             }
         }
-        else if(pil=='5')
+        else if(choose=='5')
         {
             system("clear");
-            printf("Sampai jumpa!!\n");
+            printf("Thanks for playing!\n");
             exit(EXIT_FAILURE);
             break;
         }
     }
-    pthread_join(tid[0],NULL);
-    pthread_join(tid[1],NULL);
-    pthread_join(tid[2],NULL);
-    pthread_join(tid[3],NULL);
-    pthread_join(tid[4],NULL);
-    pthread_join(tid[5],NULL);
-    pthread_join(tid[6],NULL);
+    pthread_join(thread[0],NULL);
+    pthread_join(thread[1],NULL);
+    pthread_join(thread[2],NULL);
+    pthread_join(thread[3],NULL);
+    pthread_join(thread[4],NULL);
+    pthread_join(thread[5],NULL);
+    pthread_join(thread[6],NULL);
     return 0;
 }
